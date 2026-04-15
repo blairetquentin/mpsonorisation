d215e56b607a958fc0e7a308b25e1b409d3b4ecd
blairet <blairet.quentin@gmail.com>
2026-04-15 14:29:25 +0200

    fdf

--- src/Controller/SceneController.php
+++ src/Controller/SceneController.php
@@ -18,11 +18,8 @@ class SceneController extends AbstractController
     #[Route('/scene', name: 'app_scene_index')]
     public function index(SceneRepository $sceneRepository): Response
     {
-        // On récupère l'utilisateur connecté (null si pas connecté)
         $user = $this->getUser();
 
-        // Si pas connecté, on envoie quand même le template
-        // mais avec connecte = false
         if (!$user) {
             return $this->render('scene/index.html.twig', [
                 'connecte' => false,
@@ -30,11 +27,9 @@ class SceneController extends AbstractController
             ]);
         }
 
-        // Si connecté, on récupère ses plans de scène
-        // findBy = requête avec filtre + tri
         $scenes = $sceneRepository->findBy(
-            ['user' => $user],             // WHERE user = $user
-            ['date_evenement' => 'DESC']   // ORDER BY date_evenement DESC
+            ['user' => $user],
+            ['date_evenement' => 'DESC']
         );
 
         return $this->render('scene/index.html.twig', [
@@ -42,71 +37,50 @@ class SceneController extends AbstractController
             'scenes' => $scenes,
         ]);
     }
+
     #[Route('/scene/create', name: 'app_scene_create')]
-    public function create(Request $request,EntityManagerInterface $em,InstrumentsRepository $instrumentsRepository): Response {
-        // On vérifie que l'utilisateur est connecté
-        // Si non, on le redirige vers la page de connexion
+    public function create(Request $request, EntityManagerInterface $em, InstrumentsRepository $instrumentsRepository): Response
+    {
         if (!$this->getUser()) {
             return $this->redirectToRoute('app_login');
         }
 
-        // On crée un objet Scene vide
         $scene = new Scene();
-
-        // On crée le formulaire lié à cet objet
         $form = $this->createForm(SceneType::class, $scene);
-
-        // On récupère les données envoyées par l'utilisateur (POST)
         $form->handleRequest($request);
-
-        // On récupère tous les instruments pour la section musiciens
         $instruments = $instrumentsRepository->findAll();
 
-        // Si le formulaire est soumis ET valide
         if ($form->isSubmitted() && $form->isValid()) {
-
-            // On lie la scène à l'utilisateur connecté
             $scene->setUser($this->getUser());
-
-            // Le plan commence toujours en brouillon (statut = false)
             $scene->setStatut(false);
-
-            // On sauvegarde la scène en base
             $em->persist($scene);
             $em->flush();
 
-            // On récupère les musiciens envoyés via le formulaire JS
-            // $_POST['musiciens'] = tableau de {nom, instrument_id}
             $musiciens = $request->request->all('musiciens');
 
             foreach ($musiciens as $musicien) {
-                // On vérifie qu'au moins un instrument a été sélectionné
                 if (empty($musicien['instrument_id'])) {
                     continue;
                 }
 
-                // Pour chaque instrument sélectionné on crée un ElementScene
                 foreach ($musicien['instrument_id'] as $instrumentId) {
                     $element = new ElementScene();
                     $element->setScene($scene);
                     $element->setNomMusicien($musicien['nom']);
                     $element->setQuantite(1);
-                    // Position par défaut, sera modifiée au drag & drop (SF14)
                     $element->setPositionX(0);
                     $element->setPositionY(0);
 
-                    // On récupère l'objet Instruments depuis son id
                     $instrument = $instrumentsRepository->find($instrumentId);
                     $element->setInstrument($instrument);
 
                     $em->persist($element);
                 }
             }
-            // On sauvegarde tous les ElementScene en une fois
+
             $em->flush();
 
-            // On redirige vers la page du plan (SF14)
-            return $this->redirectToRoute('app_scene_index');;
+            return $this->redirectToRoute('app_scene_index');
         }
 
         return $this->render('scene/create.html.twig', [
